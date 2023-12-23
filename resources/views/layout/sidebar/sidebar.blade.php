237d9fb5ec914c2e4c21b49e511c3ba443ca655a
munaimpro <khanmail2599@gmail.com>
2023-12-23 15:29:56 +0600

    New laravel project created

--- resources/views/layout/sidebar/sidebar.blade.php
+++ resources/views/layout/sidebar/sidebar.blade.php
@@ -7,14 +7,14 @@
                     Dashboard
                 </a>
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                    Product
+                    <div class="sb-nav-link-icon"><i class="fas fa-bus"></i></div>
+                    Trip
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
                 <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="{{ url('product') }}">All Product</a>
-                        <a class="nav-link" href="{{ url('add-product') }}">Add Product</a>
+                        <a class="nav-link" href="{{ url('trip') }}">All Trip</a>
+                        <a class="nav-link" href="{{ url('add-trip') }}">Add Trip</a>
                     </nav>
                 </div>
                 <a class="nav-link" href="{{ url('transaction') }}">
